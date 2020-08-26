462b55ad9f18b7f0fb45ad64718ff3b895a24469
jaebin <rlawoqls7@naver.com>
2020-08-26 10:44:47 +0900

    재빈 3주차 통과후 upload

--- week3_1245/jaebin.py
+++ week3_1245/jaebin.py
@@ -1,4 +1,4 @@
-
+import math  
 
 def make_dict(mc_list, n_m):
     m_list = sorted(mc_list[0:n_m])
@@ -9,7 +9,8 @@ def make_dict(mc_list, n_m):
     return m_list, m_dict 
 
 def GoldenSectionSearch(n_m, m_list, m_dict, error = 1e-12):
-    P = 0.381966
+#    P = 0.381966
+    P = (3-2.23606797749979)/2
     x_list = []
     for seg in range(n_m-1):
         max_x = m_list[seg]  #b0
@@ -33,18 +34,16 @@ def GoldenSectionSearch(n_m, m_list, m_dict, error = 1e-12):
                 test_x2_F = force_calcul(test_x2, m_list, m_dict) #f(a1)
             
             if test_x1_F > test_x2_F:   #f(b1) > f(a1)
-                if test_x2_F < error:
-                    #x = test_x2  
-                    x_list.append(round(test_x2,10))
+                if test_x2_F < error and P_len < 1e-14:
+                    x_list.append(test_x2)
                     break
                 else:
                     max_x = test_x1  
                     temp = test_x2_F  
                     flag = 1
             else:                       # f(b1) < f(a1)
-                if test_x1_F < error:
-                    #x = test_x1
-                    x_list.append(round(test_x1,10))
+                if test_x1_F < error and P_len < 1e-14:
+                    x_list.append(test_x1)
                     break
                 else:
                     min_x = test_x2  
@@ -52,15 +51,22 @@ def GoldenSectionSearch(n_m, m_list, m_dict, error = 1e-12):
                     flag = 2
             if min_val == min(test_x1_F,test_x2_F):
                 count_minval += 1
-                if count_minval == 1000:
-                    if min_val < 5e-11:
-                        x_list.append(round(test_x1,10))
+                if count_minval == 100:
+                    if min_val < 1e-10:
+                        x_list.append(test_x1) 
                     break 
             else:
                 min_val = min(test_x1_F,test_x2_F)
                 count_minval = 0
     
-    return sorted(list(set(x_list)))
+    return floor_10(sorted(list(set(x_list))))
+
+def floor_10(x_list):
+    result = list()
+    for n in x_list:   
+        result.append(format(n,".10f")) 
+    return result
+
 
 def force_calcul(x, m_list, m_dict):
     left_F = 0
@@ -82,7 +88,7 @@ for test_case in range(1, T+1):
     m_list, m_dict = make_dict(mc_list, n_m)
     result = "#"+str(test_case) 
 
-    x = GoldenSectionSearch(n_m, m_list, m_dict, error = 5e-12)
+    x = GoldenSectionSearch(n_m, m_list, m_dict, error = 1e-13)
     for i in x:
         result += " "+str(i)
     print(result)
